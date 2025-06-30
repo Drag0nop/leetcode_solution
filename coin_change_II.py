9503b5e6f210c1233517bab83598b0585198adac
Drag0n0p <nagarkotikrishna01@gmail.com>
2025-07-01 00:43:42 +0530

    Fixed Typo

--- coin_change_II.py
+++ coin_change_II.py
@@ -15,3 +15,23 @@ Explanation: there are four ways to make up the amount:
 5=1+1+1+1+1
 """
 
+from typing import List
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        cache = {}
+        def dfs(i,a):
+            if a == amount:
+                return 1
+            if a > amount:
+                return 0
+            if i == len(coins):
+                return 0
+            if (i,a) in cache:
+                return cache[(i,a)]
+            cache[(i,a)] = dfs(i,a + coins[i]) +dfs(i+1, a)
+            return cache[(i,a)]
+        return dfs(0,0)
+
+sol = Solution()
+print(sol.change(5, [1,2,5]))  # Output: 4
