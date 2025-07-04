958ff3f50f9d77f0959664abafdebf997d38caf8
Drag0n0p <nagarkotikrishna01@gmail.com>
2025-07-04 19:05:23 +0530

    Fixed Typo

--- july_2025/kth_char_str_game_2.py
+++ july_2025/kth_char_str_game_2.py
@@ -21,3 +21,36 @@ Appends "aa" to "aa", word becomes "aaaa".
 Appends "aaaa" to "aaaa", word becomes "aaaaaaaa".
 """
 
+from typing import List
+
+# 0ms  of runtime
+class Solution:
+    def kthCharacter(self, k: int, operations: List[int]) -> str:
+        k -= 1
+        bits = k.bit_length()
+        res = 0
+        for i in range(bits):
+            if (k >> i) & 1:
+                res += operations[i]
+        return chr(ord('a') + (res % 26))
+
+
+# also can be written it llike this too.(2ms of runtime)
+class Solution:
+    def kthCharacter(self, k: int, operations: List[int]) -> str:
+        k -= 1
+        num = k
+        bits = 0
+        while num:
+            num = num//2
+            bits += 1
+        res = 0
+        for i in range(bits):
+            if (k >> i) & 1:
+                res += operations[i]
+        return chr(ord('a') + (res % 26))
+
+
+
+s = Solution()
+print(s.kthCharacter(5,[0,0,0]))
