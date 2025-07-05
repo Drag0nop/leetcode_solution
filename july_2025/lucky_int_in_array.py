9b5efc92e3b6c958c099b57a94dcc757a7f36472
Drag0n0p <nagarkotikrishna01@gmail.com>
2025-07-05 22:28:36 +0530

    Fixed Typo

--- july_2025/lucky_int_in_array.py
+++ july_2025/lucky_int_in_array.py
@@ -8,3 +8,25 @@ Input: arr = [2,2,3,4]
 Output: 2
 Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
 """
+
+from typing import List
+
+# not a very much good approach(cause the complexity will be O(n^2)), the Counter-based approach is is better(O(n) complexity)
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        arr1 = []
+        for i in range(len(arr)):
+            cnt = 0
+            k = arr[i]
+            for j in range(len(arr)):
+                if arr[j] == k:
+                    cnt += 1
+            if k == cnt:
+                arr1.append(k)
+        if len(arr1) == 0:
+            return -1
+        else:
+            return max(arr1)
+
+s = Solution()
+print(s.findLucky([2,2,3,4]))
