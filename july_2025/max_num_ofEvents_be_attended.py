43fb7762c988b43ad68eecc560d47db6d6e7a6cd
Drag0n0p <nagarkotikrishna01@gmail.com>
2025-07-07 23:04:31 +0530

    Fixed Typo

--- july_2025/max_num_ofEvents_be_attended.py
+++ july_2025/max_num_ofEvents_be_attended.py
@@ -15,3 +15,27 @@ Attend the second event on day 2.
 Attend the third event on day 3.
 """
 
+# time complexity -> O(nlog(n)), space complexity -> O(n)
+
+import heapq
+from typing import List
+
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        mx = max(e for s, e in events)
+        events.sort()
+        min_heap = []
+        res = j = 0
+        for i in range(1, mx + 1):
+            while j < len(events) and events[j][0] == i:
+                heapq.heappush(min_heap, events[j][1])
+                j += 1
+            while min_heap and i > min_heap[0]:
+                heapq.heappop(min_heap)
+            if min_heap and i <= min_heap[0]:
+                heapq.heappop(min_heap)
+                res += 1
+        return res
+
+
+print(Solution().maxEvents([[1,2], [2,3], [3,4]])) # Output: 3
