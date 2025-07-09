d1fa1a85c131eb332e7f224afc6e5456643d4bb0
Drag0n0p <nagarkotikrishna01@gmail.com>
2025-07-09 18:22:03 +0530

    commit

---
+++ july_2025/reschedule_meeting_max_free_time_1.py
@@ -0,0 +1,23 @@
+"""
+You are given an integer eventTime denoting the duration of an event, where the event occurs from time t = 0 to time t = eventTime.
+
+You are also given two integer arrays startTime and endTime, each of length n. These represent the start and end time of n 
+non-overlapping meetings, where the ith meeting occurs during the time [startTime[i], endTime[i]].
+
+You can reschedule at most k meetings by moving their start time while maintaining the same duration, 
+to maximize the longest continuous period of free time during the event.
+
+The relative order of all the meetings should stay the same and they should remain non-overlapping.
+Return the maximum amount of free time possible after rearranging the meetings.
+
+Note that the meetings can not be rescheduled to a time outside the event.
+
+Example :
+
+Input: eventTime = 5, k = 1, startTime = [1,3], endTime = [2,5]
+
+Output: 2
+
+Explanation:
+Reschedule the meeting at [1, 2] to [2, 3], leaving no meetings during the time [0, 2].
+"""
