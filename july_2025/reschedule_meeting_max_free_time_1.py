fdf89de43db2b1814d898a176c2e4b11787725fa
Drag0n0p <nagarkotikrishna01@gmail.com>
2025-07-09 20:36:43 +0530

    Fixed Typo

--- july_2025/reschedule_meeting_max_free_time_1.py
+++ july_2025/reschedule_meeting_max_free_time_1.py
@@ -21,3 +21,26 @@ Output: 2
 Explanation:
 Reschedule the meeting at [1, 2] to [2, 3], leaving no meetings during the time [0, 2].
 """
+
+# complexity -> O(n)
+
+from typing import List
+
+class Solution:
+    def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
+        n= len(startTime)
+        res = 0
+
+        pre = [endTime[0] - startTime[0]]
+        for i in range(1, n):
+            pre.append(pre[-1] + endTime[i] - startTime[i])
+        
+        for i in range(k - 1, n):
+            left = endTime[i - k] if i - k >= 0 else 0
+            right = startTime[i + 1] if i + 1 < n else eventTime
+            total = pre[i] - (pre[i - k] if i - k >= 0 else 0)
+            res = max(res, right - left - total)
+        
+        return res
+
+print(Solution().maxFreeTime(5, 1, [1,3], [2,5])) #output 2
