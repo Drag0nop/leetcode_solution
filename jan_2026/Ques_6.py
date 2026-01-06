275046d1fbbdfe9e5430b7b6ef30f3dc21995d11
Drag0n0p <nagarkotikrishna01@gmail.com>
2026-01-06 21:36:39 +0530

    commit

---
+++ jan_2026/Ques_6.py
@@ -0,0 +1,49 @@
+# 1161. Maximum Level Sum of a Binary Tree
+
+# Complexity Analysis
+# Time Complexity: O(N), where N is the number of nodes in the binary tree.
+
+from collections import deque
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+        lvl = 1
+        max_sum = float('-inf')
+        res = 1
+        while q:
+            lvl_sum = 0
+            n = len(q)
+            for _ in range(n):
+                node = q.popleft()
+                lvl_sum += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if lvl_sum > max_sum:
+                max_sum = lvl_sum
+                res = lvl
+            lvl += 1
+        return res
+
+# Example usage:
+# Constructing the binary tree:
+#       1
+#      / \
+#     7   0
+#    / \
+#   7   -8
+root = TreeNode(1)
+root.left = TreeNode(7)
+root.right = TreeNode(0)
+root.left.left = TreeNode(7)
+root.left.right = TreeNode(-8)
+
+s = Solution()
+print(s.maxLevelSum(root))
