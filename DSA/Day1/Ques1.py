ef60bc83ef8c382f26566d3290c88fb08a536b9c
Drag0n0p <nagarkotikrishna01@gmail.com>
2026-07-12 12:58:56 +0530

    commit

---
+++ DSA/Day1/Ques1.py
@@ -0,0 +1,20 @@
+# Sorted Squared Array - You are given an array of Integers in which each subsequent value is not less than the previous value. 
+# Write a function that takes this array as an input and returns a new array with the squares of each number sorted in ascending order.
+
+def sorted_squared_array(arr):
+    res = [0] * len(arr)
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        left_square = arr[left] ** 2
+        right_square = arr[right] ** 2
+        if left_square > right_square:
+            res[right - left] = left_square
+            left += 1
+        else:
+            res[right - left] = right_square
+            right -= 1
+    return res
+
+# Example usage:
+input_array = [-4, -1, 0, 3, 10]
+print(sorted_squared_array(input_array))
---
+++ DSA/Day1/Ques2.py
@@ -0,0 +1,21 @@
+#  Monotonic Array - An array is monotonic if it is either monotone increasing or monotone decreasing. 
+# An array is monotone increasing if all its elements from left to right are non-decreasing. An array is monotone decreasing if all  its elements from left to right are non-increasing. 
+# Given an integer array return true if the given array is monotonic, or false otherwise.
+
+def is_monotonic(arr):
+    n = len(arr)
+    if n == 1: return True
+
+    for i in range(1, n):
+        if arr[i] < arr[i - 1]: break
+        if i == n - 1: return True
+    
+    for i in range(1, n):
+        if arr[i] > arr[i - 1]: break
+        if i == n - 1: return True
+
+    return False
+
+# Example usage:
+input_array = [1, 2, 2, 3]
+print(is_monotonic(input_array))
