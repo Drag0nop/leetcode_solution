37dcae3d3f43cc5cc54cc9377821d8265db79131
Drag0n0p <nagarkotikrishna01@gmail.com>
2026-07-14 18:46:01 +0530

    commit

--- DSA/Day2/Ques2.py
+++ DSA/Day2/Ques2.py
@@ -3,3 +3,23 @@
 # Find two lines that together with the x-axis form a container, such that the container contains the most water(depth is constant across containers). 
 # Return the area(that the 2 lines and the X axis make) of container which can store the max amount of water. 
 # Notice that you may not slant the container.
+
+def maxArea(height):
+    left = 0
+    right = len(height) - 1
+    max_area = 0
+
+    while left < right:
+        width = right - left
+        current_area = width * (min(height[left], height[right]))
+        max_area = max(max_area, current_area)
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_area
+
+# Example usage:
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+print(maxArea(height))  # Output: 49
