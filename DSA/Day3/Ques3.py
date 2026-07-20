59c39e269750a1f582214369b09221777bcecf9b
Drag0n0p <nagarkotikrishna01@gmail.com>
2026-07-20 23:54:57 +0530

    commit

--- DSA/Day3/Ques3.py
+++ DSA/Day3/Ques3.py
@@ -1,2 +1,20 @@
 # Permutations: Given an array nums of distinct integers, return all the possible permutations. 
 # You can return the answer in any order.
+
+def permutations(nums):
+    res = []
+    def backtrack(path):
+        if len(path) == len(nums):
+            res.append(path[:])
+            return
+        for i in range(len(nums)):
+            if nums[i] not in path:
+                path.append(nums[i])
+                backtrack(path)
+                path.pop()
+    backtrack([])
+    return res
+
+# Example usage:
+nums = [1, 2, 3]
+print(permutations(nums))
