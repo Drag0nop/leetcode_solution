74ee8d57002db2d7a3e59c91610a5acad45947cd
Drag0n0p <nagarkotikrishna01@gmail.com>
2026-09-15 12:27:03 +0530

    commit

--- dp_in_Cpp/maxPathSum.cpp
+++ dp_in_Cpp/maxPathSum.cpp
@@ -39,12 +39,62 @@ int tabulationMaxPath(vector<vector<int>>& arr, int m, int n) {
     for (int i = 1; i < m; i++) {
         for (int j = 0; j < n; j++) {
             int s = arr[i][j] + dp[i - 1][j];
-            int ld = (j > 0) ? arr[i][j] + dp[i - 1][j - 1] : INT_MIN;
-            int rd = (j < n - 1) ? arr[i][j] + dp[i - 1][j + 1] : INT_MIN;
+            int ld = arr[i][j];
+            if (j - 1 >= 0) {
+                ld += dp[i - 1][j - 1];
+            }
+            else {
+                ld += INT_MIN;
+            }
+            int rd = arr[i][j];
+            if (j + 1 < n) {
+                rd = rd + dp[i - 1][j + 1];
+            }
+            else {
+                rd += INT_MIN;
+            }
             dp[i][j] = max(s, max(ld, rd));
         }
     }
-    return *max_element(dp[m - 1].begin(), dp[m - 1].end());
+    int res = INT_MIN;
+    for (int j = 0; j < n; j++) {
+        res = max(res, dp[m - 1][j]);
+    }
+    return res;
+}
+
+int optimizationMaxPath(vector<vector<int>>& arr, int m, int n) {
+    vector<int> prev(n, 0);
+    for (int j = 0; j < n; j++) {
+        prev[j] = arr[0][j];
+    }
+    for (int i = 1; i < m; i++) {
+        vector<int> curr(n, 0);
+        for (int j = 0; j < n; j++) {
+            int s = arr[i][j] + prev[j];
+            int ld = arr[i][j];
+            if (j - 1 >= 0) {
+                ld += prev[j - 1];
+            }
+            else {
+                ld += INT_MIN;
+            }
+            int rd = arr[i][j];
+            if (j + 1 < n) {
+                rd = rd + prev[j + 1];
+            }
+            else {
+                rd += INT_MIN;
+            }
+            curr[j] = max(s, max(ld, rd));
+        }
+        prev = curr;
+    }
+    int res = INT_MIN;
+    for (int j = 0; j < n; j++) {
+        res = max(res, prev[j]);
+    }
+    return res;
 }
 int main() {
     int n, m;
@@ -59,5 +109,6 @@ int main() {
     vector<vector<int>> dp(m, vector<int>(n, -1));
     cout << memorizationMaxPath(m - 1, n - 1, arr, dp) << endl;
     cout << tabulationMaxPath(arr, m, n) << endl;
+    cout << optimizationMaxPath(arr, m, n) << endl;
     return 0;
 }
